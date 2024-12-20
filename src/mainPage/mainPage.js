8737350edb98c8eb15bf9033798df62962bb9044
Maurício Grigoleto <102479476+mgrigoleto@users.noreply.github.com>
2024-12-19 23:19:13 -0300

    Update mainPage.js

--- src/mainPage/mainPage.js
+++ src/mainPage/mainPage.js
@@ -224,7 +224,7 @@ export default function Builder(){
                 </div>  
                 <div style={{display:'flex', flexDirection:'column', textAlign:'right'}}>
                     <b style={{margin:'0 0 10px 0'}}>Developed by <a target='_blank' rel='noreferrer' href='https://steamcommunity.com/id/bodd3'>Bodd3</a></b>  
-                    <b>Version 0.4.0</b>  
+                    <b>Version 0.4.1</b>  
                 </div>             
             </div>
         </div>
